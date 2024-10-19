942149369a07435ad8ae7b6efe5b4633594f8cf7
Dimitrius McKinnon <113384104+Dimitrius504@users.noreply.github.com>
2024-10-19 14:55:01 -0400

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,14 +1,1 @@
-import express from "express";
-const router = express.Router();
-
-// Get all items
-router.get('/items', async (req, res) => {
-  res.json('cum');
-});
-
-// Create a new item
-router.post('/items', async (req, res) => {
- console.log('you came');
-});
-
-export default router;
+// Unnecessary file
